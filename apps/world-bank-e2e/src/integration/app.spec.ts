edc7c9c0507f462c44a827295849aed06b84698c
Enol Casielles Martinez <enolcasielles30@hotmail.com>
2019-11-16 00:18:39 +0100

    crea test de integraciom

--- apps/world-bank-e2e/src/integration/app.spec.ts
+++ apps/world-bank-e2e/src/integration/app.spec.ts
@@ -1,9 +1,22 @@
-import { getGreeting } from '../support/app.po';
+import { getTitle } from '../support/app.po';
 
-describe('world-bank', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome to world-bank!');
-  });
+describe('GIVEN: world-bank app', () => {
+  describe('WHEN: user visit home page', () => {
+    beforeEach(() => cy.visit('/'));
+    it(`should display 'Regiones' title`, () => {
+      getTitle().contains('Regiones');
+    });
+  })
+  describe('WHEN: user visit region page', () => {
+    beforeEach(() => cy.visit('/region/EAS'));
+    it(`should display 'East Asia & Pacific' title`, () => {
+      getTitle().contains('East Asia & Pacific');
+    });
+  })
+  describe('WHEN: user visit country page', () => {
+    beforeEach(() => cy.visit('/country/MAC'));
+    it(`should display 'Macao SAR, China' title`, () => {
+      getTitle().contains('Macao SAR, China');
+    });
+  })
 });
